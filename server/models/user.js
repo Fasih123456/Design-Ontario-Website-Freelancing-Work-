8b0b9146c5eaac74c71e9891ff8db11840105145
Fasih <fasihislam1999@gmail.com>
2022-08-22 17:15:15 -0300

    fixed some env file issusses

--- server/models/user.js
+++ server/models/user.js
@@ -3,6 +3,7 @@ const jwt = require("jsonwebtoken");
 const Joi = require("joi");
 const passwordComplexity = require("joi-password-complexity");
 
+
 const userSchema = new mongoose.Schema({
 	firstName: { type: String, required: true },
 	lastName: { type: String, required: true },
@@ -14,6 +15,7 @@ userSchema.methods.generateAuthToken = function () {
 	const token = jwt.sign({ _id: this._id }, process.env.JWTPRIVATEKEY, {
 		expiresIn: "7d",
 	});
+
 	return token;
 };
 
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,13 +3,17 @@ const { User } = require("../models/user");
 const bcrypt = require("bcrypt");
 const Joi = require("joi");
 
+
 router.post("/", async (req, res) => {
+	
 	try {
+		
 		const { error } = validate(req.body);
 		if (error)
 			return res.status(400).send({ message: error.details[0].message });
 
 		const user = await User.findOne({ email: req.body.email });
+		console.log(user)
 		if (!user)
 			return res.status(401).send({ message: "Invalid Email or Password" });
 
@@ -17,12 +21,16 @@ router.post("/", async (req, res) => {
 			req.body.password,
 			user.password
 		);
+		
+
 		if (!validPassword)
 			return res.status(401).send({ message: "Invalid Email or Password" });
 
 		const token = user.generateAuthToken();
+
 		res.status(200).send({ data: token, message: "logged in successfully" });
 	} catch (error) {
+		
 		res.status(500).send({ message: "Internal Server Error" });
 	}
 });
