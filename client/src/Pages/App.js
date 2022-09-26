7427818f13af94bf4e6b755d6112a61236609a08
Fasih <fasihislam1999@gmail.com>
2022-09-26 16:32:44 -0300

    revamped website

--- client/src/Pages/App.js
+++ client/src/Pages/App.js
@@ -1,32 +1,29 @@
-import React from 'react';
+import React from "react";
 
-import { BrowserRouter, Route, Routes, Navigate, Link } from 'react-router-dom';
-import Work from './Work';
-import Contact from './Contact';
-import costCalculations from './costCalculations';
-import Main from '../Components/Main';
-import Login from '../Components/Login';
-import Signup from '../Components/Signup'
-import Home from './Home';
-import "./css/index.css"
-import "./css/main.css"
+import { BrowserRouter, Route, Routes, Navigate, Link } from "react-router-dom";
+import Work from "./Work";
+import Contact from "./Contact";
+import costCalculations from "./costCalculations";
+import Main from "../Components/Main";
+import Login from "../Components/Login";
+import Signup from "../Components/Signup";
+import Home from "./Home";
+import "./css/index.css";
+import "./css/main.css";
 
 const App = () => {
-  const user = localStorage.getItem("token")
-    return(
-        <div className='ui container'>
-
-            <Routes>
-              {user && <Route path="/" excat element={<Main />} />}
-              <Route path="/signup" excat element={<Signup />} />
-              <Route path="/login" excat element={<Login />} />
-              <Route path="/" excat element={<Home />} />
-              <Route path="/work" excat element={<Work />} />
-              <Route path="/contact" excat element={<Contact />} />
-              <Route path="/cost" excat element={<costCalculations />} />
-            </Routes>
-        </div>
-    );
+  const user = localStorage.getItem("token");
+  return (
+    <Routes>
+      {user && <Route path="/" excat element={<Main />} />}
+      <Route path="/signup" excat element={<Signup />} />
+      <Route path="/login" excat element={<Login />} />
+      <Route path="/" excat element={<Home />} />
+      <Route path="/work" excat element={<Work />} />
+      <Route path="/contact" excat element={<Contact />} />
+      <Route path="/cost" excat element={<costCalculations />} />
+    </Routes>
+  );
 };
 
 export default App;
