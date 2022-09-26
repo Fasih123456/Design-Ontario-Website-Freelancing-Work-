ba6a51b69f542841df2a9074ceee74859644489d
Fasih <fasihislam1999@gmail.com>
2022-09-26 13:56:12 -0300

    version 1

--- index.js
+++ index.js
@@ -51,5 +51,5 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 9000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
